ef0ca23124fc031a89e21b76582c1d869a8c1413
Caio Lemos <caioledan@gmail.com>
2026-03-13 16:33:34 -0300

    adding delete professor

--- src/routes/professor.routes.ts
+++ src/routes/professor.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createProfessor, viewAllProfessors } from "../controllers/professor.controller.js";
+import { createProfessor, deleteProfessor, viewAllProfessors } from "../controllers/professor.controller.js";
 
 const router = Router();
 
 router.post("/register", createProfessor)
-
 router.get("/get", viewAllProfessors)
+router.delete("/delete", deleteProfessor)
 
 export default router;
